1efab1e04c01c843f5dace076a7f8040c31193d7
ddl <ddl@novatec-gmbh.de>
2022-03-17 15:04:12 +0100

    feature: 2 streams inside mergeMap

--- merge_map_operator.js
+++ merge_map_operator.js
@@ -1,9 +1,10 @@
-import {from, of, pipe, interval, mergeMap} from 'rxjs';
+import {from, of, pipe, map, interval, mergeMap} from 'rxjs';
 const strm1 = interval(1000);
+const strm2 = of("El stream emite -> ");
 
     strm1
       .pipe(
         mergeMap(
-          (vMap1) => of("El stream emite -> "+vMap1)
+          (valor1) => strm2.pipe(map(valor2 => valor2+valor1))
         )
       ).subscribe(r => console.log(r));
