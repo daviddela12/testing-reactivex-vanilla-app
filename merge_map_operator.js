ddf98020b78672976ff2841c0e19a99a9ff26690
ddl <ddl@novatec-gmbh.de>
2022-03-17 15:09:29 +0100

    feature: 2 streams inside mergeMap

--- merge_map_operator.js
+++ merge_map_operator.js
@@ -3,6 +3,8 @@ const strm1 = interval(1000);
 const strm2 = of("El stream emite -> ");
 
 //MergeMap es un operador nos permite unificar en un solo Observable dos flujos de datos, haciendo que de dos entradas obtengamos una salida “mergeada”.
+
+// al final es como si dijeras Cuando se genere el strm1 que se genere también el strm2
     strm1
       .pipe(
         mergeMap(
