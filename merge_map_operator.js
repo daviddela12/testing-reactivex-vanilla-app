ebf055b356ccac60efb6665eb010c02fe9460a6d
ddl <ddl@novatec-gmbh.de>
2022-03-17 15:23:09 +0100

    feature: new example mergeMap

--- merge_map_operator.js
+++ merge_map_operator.js
@@ -1,13 +1,30 @@
-import {from, of, pipe, map, interval, mergeMap} from 'rxjs';
+import {from, of, pipe, map, interval, mergeMap, timer} from 'rxjs';
 const strm1 = interval(1000);
 const strm2 = of("El stream emite -> ");
 
 //MergeMap es un operador nos permite unificar en un solo Observable dos flujos de datos, haciendo que de dos entradas obtengamos una salida “mergeada”.
 
-// al final es como si dijeras Cuando se genere el strm1 que se genere también el strm2
+// al final es como si dijeras Cuando se emita el strm1 que se emita también el strm2
     strm1
       .pipe(
         mergeMap(
           (valor1) => strm2.pipe(map(valor2 => valor2+valor1))
         )
       ).subscribe(r => console.log(r));
+
+
+      /**
+       * vamos a emular un click cada 1s(1000ms), pero vamos a controlar las llamadas asíncronas y solo permitiremos coger cinco de ellas simultaneamente. 
+       * Posteriormente, conforme vayan terminando unas, irán entrando otras, pero siempre como máximo activas cinco. 
+       * El primer código muestra esta funcionalidad, pero como observaras solo muestra información por la consola cuando termina una tarea.
+       */
+      const obs1 = interval(1000);
+      const obs2 = timer(5000);
+  
+      obs1
+        .pipe(
+          mergeMap(
+            (vMap1) => obs2.pipe(map(w=>"Hilo terminado -> " + vMap1)),
+            5
+          )
+        ).subscribe(r => console.log(r));
